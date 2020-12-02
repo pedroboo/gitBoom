0e2c26e63f6520c7eebd862bd8486bbcb422a117
pedroboo <71462037+pedroboo@users.noreply.github.com>
2020-12-01 21:32:59 -0300

    Add files via upload

---
+++ View/Vendedores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade01.View
+{
+    class Vendedores
+    {
+
+        private readonly Vendedores[] osVendedores;
+        private int max;
+        private int qtde;
+
+        public int Max { get => max; set => max = value; }
+        public int Qtde { get => qtde; set => qtde = value; }
+        
+    
+    }
+}
